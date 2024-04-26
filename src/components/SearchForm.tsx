a8c178252d46ba3b9a2d512c48ec328d631a2f2d
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-04-26 11:37:26 +0300

    fetch data in useEffect

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -2,9 +2,20 @@ import { useEffect, useState } from 'react';
 
 export default function SearchForm() {
   const [searchText, setSearchText] = useState('');
+  const [jobItems, setJobItems] = useState([]);
+  console.log(jobItems);
 
   useEffect(() => {
     if (!searchText) return;
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+      );
+      const data = await response.json();
+      setJobItems(data.jobItems);
+    };
+
+    fetchData();
   }, [searchText]);
 
   return (
