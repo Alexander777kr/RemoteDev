8ebd0b2b42bb4c5166b5053633374f99db32d1f4
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-04-27 14:37:36 +0300

    slice JobItems (Derived state)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Background from './Background';
 import Container from './Container';
 import Footer from './Footer';
@@ -16,7 +16,7 @@ import { useJobItems } from '../lib/hooks';
 
 function App() {
   const [searchText, setSearchText] = useState('');
-  const { jobItems, isLoading } = useJobItems(searchText);
+  const { jobItemsSliced, isLoading } = useJobItems(searchText);
 
   return (
     <>
@@ -36,7 +36,7 @@ function App() {
             <ResultsCount />
             <SortingControls />
           </SidebarTop>
-          <JobList jobItems={jobItems} isLoading={isLoading} />
+          <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
           <PaginationControls />
         </Sidebar>
         <JobItemContent />
