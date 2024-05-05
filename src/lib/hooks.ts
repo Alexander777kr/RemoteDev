4042dd0114de58663d1fcb0224674878b912212b
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-05-05 15:29:23 +0300

    context API Typescript (custom hook)

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { JobItem, JobItemExpanded } from "./types";
 import { BASE_API_URL } from "./constants";
 import { useQuery } from "@tanstack/react-query";
 import { handleError } from "./utils";
+import { BookmarksContext } from "../contexts/BookmarksContextProvider";
 
 type JobItemApiResponse = {
   public: boolean;
@@ -111,4 +112,15 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, React.Disp
   }, [value, key]);
 
   return [value, setValue] as const;
+}
+
+
+export function useBookmarksContext() {
+  const context = useContext(BookmarksContext);
+  if (!context) {
+    throw new Error(
+      'useBookmarksContext must be used within a BookmarksContextProvider'
+    );
+  }
+  return context;
 }
